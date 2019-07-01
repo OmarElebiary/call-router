924b792c0be25bff4fb8c380a14674bb5f8b587c
Omar Elebiary <ome@mybingli.com>
2019-07-01 09:44:02 +0200

    Adding the call router class

--- call-routing/CallRouter.cs
+++ call-routing/CallRouter.cs
@@ -1,7 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace call_routing
 {
     public class CallRouter
     {
-        
+        // Function to receive the phone number and Dictionary containing the operators and operator rates per country code
+        public static string FindCheapestOperator(string phoneNumber, Dictionary<string, Dictionary<string, string>> operatorsList)
+        {
+            if (string.IsNullOrEmpty(phoneNumber)) return string.Empty;
+            string currentCheapestOperator = string.Empty;
+            float currentCallRate = float.MaxValue;
+
+            foreach (KeyValuePair<string,Dictionary<string,string>> callOperator in operatorsList)
+            {
+                // Find all country codes that start with the same number instead of iterating over the whole country codes list
+                var operatorRates = callOperator.Value.Where(s => s.Key.StartsWith(phoneNumber.ElementAt(0)));
+                var keyValuePairs = operatorRates as KeyValuePair<string, string>[] ?? operatorRates.ToArray();
+                if (!keyValuePairs.Any()) continue;
+
+                // Finding the longest country code that matches the given number
+                // I used this way because I don't know exactly the country code length in a given number
+                int currentSubstringMax = FindAccurateKey(phoneNumber, keyValuePairs);
+                if (!callOperator.Value.ContainsKey(phoneNumber.Substring(0, currentSubstringMax))) continue;
+                float callRate = float.Parse(callOperator.Value[phoneNumber.Substring(0, currentSubstringMax)]);
+                if (!(callRate <= currentCallRate)) continue;
+                currentCallRate = callRate;
+                currentCheapestOperator = callOperator.Key;
+            }
+            return currentCheapestOperator;
+        }
+
+        // This function is used to find the country code that is used in a given phone number
+        private static int FindAccurateKey(string phoneNumber, IEnumerable<KeyValuePair<string, string>> operatorRates)
+        {
+            int count = 0, maxLen = 0;
+            foreach (KeyValuePair<string,string> rate in operatorRates)
+            {
+                int lenA = phoneNumber.Length, lenB = rate.Key.Length;
+                if (lenA == 0 || lenB == 0) return count;
+                count = LongestCommonKey(lenA, lenB, 0, phoneNumber, rate.Key);
+                if (count >= maxLen) maxLen = count;
+            }
+
+            return maxLen;
+        }
+
+        // A function used to find the longest common substring in the phone number which matches the country code in an operator list and return the length of the country code
+        private static int LongestCommonKey(int lenA, int lenB, int count, string phoneNumber, string callKey)
+        {
+            if (lenA == 0 || lenB == 0) return count;
+            if (phoneNumber[lenA - 1] == callKey[lenB - 1])
+            {
+                count = LongestCommonKey(lenA - 1, lenB - 1, count + 1, phoneNumber, callKey);
+            }
+
+            count = Math.Max(count,
+                Math.Max(LongestCommonKey(lenA, lenB - 1, 0, phoneNumber, callKey),
+                    LongestCommonKey(lenA - 1, lenB, 0, phoneNumber, callKey)));
+            return count;
+        }
     }
 }
--- call-routing/Program.cs
+++ call-routing/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace call_routing
 {
@@ -6,7 +7,34 @@ namespace call_routing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // For testing purposes
+            // Initiating a new dict containing operators
+            var operatorsList =
+                new Dictionary<string, Dictionary<string, string>>
+                {
+                    ["Operator A"] = new Dictionary<string, string>
+                    {
+                        {"1", "0.9"},
+                        {"268", "5.1"},
+                        {"46", "0.17"},
+                        {"4620", "0.0"},
+                        {"468", "0.15"},
+                        {"4631", "0.15"},
+                        {"4673", "0.9"},
+                        {"46732", "1.1"}
+                    },
+                    ["Operator B"] = new Dictionary<string, string>
+                    {
+                        {"1", "0.92"},
+                        {"44", "0.5"},
+                        {"46", "0.2"},
+                        {"467", "1.0"},
+                        {"48", "1.2"}
+                    }
+                };
+
+            string cheapestOperator = CallRouter.FindCheapestOperator("4673212345", operatorsList);
+            Console.WriteLine("Cheapest operator is " + cheapestOperator);
         }
     }
 }
